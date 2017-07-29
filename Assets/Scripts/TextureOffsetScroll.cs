a3a961119d272501fff3538b94b13956eacad82c
Sava Minić <minic.sava@gmail.com>
2017-07-29 22:34:31 +0200

    parallax

---
+++ Assets/Scripts/TextureOffsetScroll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureOffsetScroll : MonoBehaviour
+{
+
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+
+	private Renderer myRenderer;
+
+	void Start()
+	{
+		myRenderer = GetComponent<Renderer>();
+		savedOffset = myRenderer.sharedMaterial.GetTextureOffset("_MainTex");
+	}
+
+	void Update()
+	{
+		float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2(x, savedOffset.y);
+		myRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+
+	void OnDisable()
+	{
+		myRenderer.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+	}
+}
