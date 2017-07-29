fe8135776026a5f0cd1df4f74cf09cbce2f30e7d
Sava Minić <minic.sava@gmail.com>
2017-07-29 15:56:42 +0200

    added camera controller, update train, fixed colliders a little

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public float minPosition = -5.6f;
+	public float maxPosition = 6.25f;
+
+	public float speed = 10f;
+
+	private Vector3 offset;
+	private Vector3 playerPosition { get { return GameManager.Instance.player.transform.position; } }
+
+	void Start()
+	{
+		offset = transform.position - playerPosition;
+	}
+
+	void LateUpdate () 
+	{
+		var newPosition = playerPosition + offset;
+		var x = Mathf.Clamp(newPosition.x, minPosition, maxPosition);
+		newPosition = new Vector3(x, newPosition.y / 10f, transform.position.z);
+
+		transform.position = Vector3.Lerp(transform.position, newPosition, speed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,26 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
 
+	#region Singleton
+
+	private static GameManager instance;
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = FindObjectOfType<GameManager>();
+			}
+			return instance;
+		}
+	}
+
+	#endregion
+
+	public PlayerController player;
+
 	void Awake()
 	{
 		Application.targetFrameRate = 60;
